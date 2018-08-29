229cd0424082661ad3e97788d3ddbab2b3724a2b
zlao <2269648132@qq.com>
2018-08-29 10:49:32 +0800

    :smile: spring-cloud-demo

--- demo-eureka-springboot-admin/src/main/java/com/example/demo/DemoEurekaSpringbootAdminApplication.java
+++ demo-eureka-springboot-admin/src/main/java/com/example/demo/DemoEurekaSpringbootAdminApplication.java
@@ -3,10 +3,12 @@ package com.example.demo;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
 @EnableAdminServer
 public class DemoEurekaSpringbootAdminApplication {
 
